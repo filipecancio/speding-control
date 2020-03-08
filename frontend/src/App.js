aaa7187c67918df1691a18fb5e51b0a9f003da83
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-08 19:06:54 -0300

    :construction: Configurar dados inciais do backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <>
       <h1>Desafio Nubank</h1>
+      <div>
+      <h1>Bruna Jullyeti Viana de Jesus</h1>
+      <h2>R$20,25</h2>
+      </div>
     </>
   );
 }
