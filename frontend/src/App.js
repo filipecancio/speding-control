b36e05761f3ebcd87e38c38f2b756eb43b215d9c
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-08 20:53:17 -0300

    :heavy_plus_sign: Add eslint e prettier dependecies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <>
       <h1>Desafio Nubank</h1>
       <div>
-      <h1>Bruna Jullyeti Viana de Jesus</h1>
-      <h2>R$20,25</h2>
+        <h1>Bruna Jullyeti Viana de Jesus</h1>
+        <h2>R$20,25</h2>
       </div>
     </>
   );
