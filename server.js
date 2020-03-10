a0d5b6dfcd33fe18e93dd922fb7f57f2489a1b20
jgguida <jon.gui@aol.com>
2020-03-10 16:49:29 -0300

    New routes:sparkles:

--- server.js
+++ server.js
@@ -9,13 +9,28 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
     var dados = 
-      { "Nome" : "Bruna Jullyeti Viana de Jesus", "Saldo" : "R$20,25" }
-      
-    ;
+      { "Nome" : "Bruna Jullyeti Viana de Jesus", "Saldo" : "R$20,25" };
   
     res.send(JSON.stringify(dados));
   });
 
+  app.get('/contas', function(req, res) {
+    var contas = 
+      {  Energia : 80,
+      Água : 62.50 };
+      
+    
+      res.send(JSON.stringify(contas));
+  });
+
+  app.get('/alimentar', function(req, res) {
+    var gastos = 
+      { Fev : '67%',
+        Mar: '38%'};
+      
+    
+      res.send(JSON.stringify(gastos));
+  });
   app.listen(8001, function() {
-    console.log('Servidor rodando na porta 8000.');
+    console.log('Servidor rodando na porta 8001.');
   });
