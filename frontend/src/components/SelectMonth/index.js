5ba22e0bb4cb1249685a5415f45c9894fb8414a2
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-11 02:15:50 -0300

    💄 Update Page 04

---
+++ frontend/src/components/SelectMonth/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ButtonBack from "../ButtonBack";
+
+function SelectMonth(props) {
+  return (
+    <>
+      <ButtonBack />
+      <h1>FEV</h1>
+      <h1>MAR</h1>
+      <h1>ABR</h1>
+      <ButtonBack />
+    </>
+  );
+}
+
+export default SelectMonth;
