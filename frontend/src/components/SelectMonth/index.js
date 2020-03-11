01e52a2aa0983de41edfd0a36c79a9243d4a4c7e
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-11 02:39:22 -0300

    💄 Update Page 04

--- frontend/src/components/SelectMonth/index.js
+++ frontend/src/components/SelectMonth/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ButtonBack from "../ButtonBack";
+import ButtonNext from "../ButtonNext";
+import { Content } from "./styles";
 
 function SelectMonth(props) {
   return (
     <>
-      <ButtonBack />
-      <h1>FEV</h1>
-      <h1>MAR</h1>
-      <h1>ABR</h1>
-      <ButtonBack />
+      <Content>
+        <ButtonBack />
+        <h1>FEV</h1>
+        <h1>MAR</h1>
+        <h1>ABR</h1>
+        <ButtonNext />
+      </Content>
     </>
   );
 }
--- frontend/src/components/SelectMonth/styles.js
+++ frontend/src/components/SelectMonth/styles.js
@@ -2,19 +2,9 @@ import styled from "styled-components";
 import icon from "../../icons/icon-grafico-02.svg";
 
 export const Content = styled.div`
-  background-color: var(--color-third-300);
   width: 100%;
-  height: 200px;
-  border-radius: 2px;
 
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-`;
-
-export const Icon = styled.div`
-  background: url(${icon}) no-repeat center right;
-  width: 90%;
-  height: 300px;
 `;
