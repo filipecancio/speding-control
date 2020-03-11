3074be48c9892ed2e0c30d47f74f57709399098c
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-11 02:39:43 -0300

    💄 Update Page 04

---
+++ frontend/src/components/ErrorIcon/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Content, Icon } from "./styles";
+
+function ErrorIcon(props) {
+  return (
+    <>
+      <Content>
+        <Icon />
+      </Content>
+    </>
+  );
+}
+
+export default ErrorIcon;
