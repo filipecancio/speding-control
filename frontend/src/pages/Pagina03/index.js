eec810a4bc78a69ef8c1fc43a5b1638375339cf6
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-19 02:39:26 -0300

    Integrating all the pages on backend

--- frontend/src/pages/Pagina03/index.js
+++ frontend/src/pages/Pagina03/index.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import BadgeTransaction from "../../components/BadgeTransaction";
 import api from "./data.json";
 
 function Pagina03() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    (async () => {
+      axios
+        .get(`https://us-central1-speding-control.cloudfunctions.net/api/pg03`)
+        .then(res => {
+          const response = res.data;
+          console.log(response);
+          setData(response);
+        });
+    })();
+  }, []);
   return (
     <div>
       <h1>{api.value}</h1>
--- frontend/src/pages/Pagina04/index.js
+++ frontend/src/pages/Pagina04/index.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import SelectMonth from "../../components/SelectMonth";
 import TotalTransaction from "../../components/TotalTransaction";
 import Graph01 from "../../components/Graph01";
 import api from "./data.json";
 
 function Pagina04() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    (async () => {
+      axios
+        .get(`https://us-central1-speding-control.cloudfunctions.net/api/pg04`)
+        .then(res => {
+          const response = res.data;
+          console.log(response);
+          setData(response);
+        });
+    })();
+  }, []);
   return (
     <div>
       <h1>{api.value}</h1>
