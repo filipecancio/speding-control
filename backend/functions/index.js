78df305e435fac542e0992239ed754545c88bf4d
Filipe Câncio <filipe.cancio@outlook.com>
2020-03-19 02:20:50 -0300

    🚧Finishing the firebase integration

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,8 +1,38 @@
-const functions = require('firebase-functions');
+/* eslint-disable promise/always-return */
+/* eslint-disable promise/catch-or-return */
+const functions = require("firebase-functions");
+const app = require("express")();
+const admin = require("firebase-admin");
+const corsModule = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const cors = corsModule({ origin: true });
+
+//Initialize the firebase functions
+admin.initializeApp();
+
+//Initialize the firestore
+const db = admin.firestore().collection("pg01");
+
+app.get("/pg01", (request, response) => {
+  cors(request, response, async () => {
+    db.get().then(docs => {
+      let base = [];
+
+      docs.forEach(doc => {
+        base.push(doc.data());
+      });
+
+      response.json(base);
+    });
+  });
+});
+app.post("/pg01", (request, response) => {
+  cors(request, response, async () => {
+    const newBase = request.body;
+    db.add(newBase).then(() => {
+      response.status(200).json(null);
+    });
+  });
+});
+
+exports.api = functions.https.onRequest(app);
